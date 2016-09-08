c4bcea989f9d1b74a9876833e878073d1d224c76
Sean Michael <smichael@kenzan.com>
2016-09-08 14:55:02 -0600

    ng2-webpack initial commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  entry: {
+    polyfills: './src/polyfills.ts',
+    app: './src/main.ts'
+  },
+  output: {
+    path: './dist/',
+    filename: '[name].bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  }
+};
