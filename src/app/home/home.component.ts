aef0337ea2c9dfa0d8cba35b53185d732e051569
Sean Michael <seankmichael@gmail.com>
2016-09-10 23:04:46 -0600

    start working on home from blank tests

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,9 @@
 import { Component } from '@angular/core';
 
-import { AppState } from '../app.service';
-
 @Component({
-  selector: 'home',
-  providers: [ ],
-  directives: [ ],
-  pipes: [ ],
   styleUrls: [ './home.style.scss' ],
   templateUrl: './home.template.html'
 })
 export class HomeComponent {
-  // Set our default values
-  localState = { value: '' };
-  // TypeScript public modifiers
-  constructor(public appState: AppState) {
-
-  }
-
-  ngOnInit() {
-    console.log('hello `Home` component');
-    // this.title.getData().subscribe(data => this.data = data);
-  }
-
-  submitState(value) {
-    console.log('submitState', value);
-    this.appState.set('value', value);
-    this.localState.value = '';
-  }
 
 }
